2af9d572f57105103250e4da83494082f4a1d3e5
gara501 <gandres.ramirez@gmail.com>
2018-11-07 23:51:05 -0500

    First Code

---
+++ js/game.js
@@ -0,0 +1,45 @@
+const config = {
+  type: Phaser.AUTO, // Which renderer to use
+  width: 800, // Canvas width in pixels
+  height: 600, // Canvas height in pixels
+  backgroundColor: "#333333",
+  parent: "game-container", // ID of the DOM element to add the canvas to
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  // Runs once, loads up assets like images and audio
+  this.load.image("repeating-background", "/images/bg.jpg");
+  this.load.image("mario-tiles", "/images/mario.png");
+}
+
+function create() {
+  // Runs once, after all assets in preload are loaded
+  const { width, height } = this.sys.game.config;
+  const level = [
+    [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+    [  0,   1,   2,   3,   0,   0,   0,   1,   2,   3,   0 ],
+    [  0,   5,   6,   7,   0,   0,   0,   5,   6,   7,   0 ],
+    [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+    [  0,   0,   0,  14,  13,  14,   0,   0,   0,   0,   0 ],
+    [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+    [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+    [  0,   0,  14,  14,  14,  14,  14,   0,   0,   0,  15 ],
+    [  0,   0,   0,   0,   0,   0,   0,   0,   0,  15,  15 ],
+    [ 35,  36,  37,   0,   0,   0,   0,   0,  15,  15,  15 ],
+    [ 39,  39,  39,  39,  39,  39,  39,  39,  39,  39,  39 ]
+  ];
+  const map = this.make.tilemap({ data: level, tileWidth: 16, tileHeight: 16 });
+  const tiles = map.addTilesetImage("mario-tiles");
+  const layer = map.createStaticLayer(0, tiles, 0, 0);
+}
+
+function update(time, delta) {
+  // Runs once per frame for the duration of the scene
+}
